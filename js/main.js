c5361748e9c776c80681e4898b744630a1707b2e
Fernando Siqueira <fernandosiqueira994@gmail.com>
2024-05-01 04:47:13 -0300

    incluindo event listeners para os links de email, whats, linkedin e github

---
+++ js/main.js
@@ -0,0 +1,19 @@
+// Event listener para o link do email
+document.getElementById('mailadd').addEventListener('click', function () {
+    console.log('Usuário clicou no link de email.');
+});
+
+// Event listener para o link do WhatsApp
+document.getElementById('whatsapp').addEventListener('click', function () {
+    console.log('Usuário clicou no link do WhatsApp.');
+});
+
+// Event listener para o link do LinkedIn
+document.getElementById('linkedin').addEventListener('click', function () {
+    console.log('Usuário clicou no link do LinkedIn.');
+});
+
+// Event listener para o link do GitHub
+document.getElementById('github').addEventListener('click', function () {
+    console.log('Usuário clicou no link do GitHub.');
+});
